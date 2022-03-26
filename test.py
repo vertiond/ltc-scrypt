4ceabba8cf11fb3a3e750e44831ab1c5d279a43e
vertiond <vertion@protonmail.com>
2022-03-25 22:45:41 -0500

    Add test and use Github actions to build

---
+++ test.py
@@ -0,0 +1,19 @@
+import sys
+
+import ltc_scrypt
+
+teststart = '000000203a297b4b7685170d7644b43e5a6056234cc2414edde454a87580e1967d14c1078c13ea916117b0608732f3f65c2e03b81322efc0a62bcee77d8a9371261970a58a5a715da80e031b02560ad8'
+
+if sys.version_info[0] < 3:
+    testbin = teststart.decode('hex')
+else:
+    testbin = bytes.fromhex(teststart)
+
+
+hash_bin_scrypt = ltc_scrypt.getPoWHash(testbin)
+
+if sys.version_info[0] < 3:
+    print("ltc_scrypt", hash_bin_scrypt.encode('hex'))
+
+else:
+    print("ltc_scrypt", hash_bin_scrypt.hex())
